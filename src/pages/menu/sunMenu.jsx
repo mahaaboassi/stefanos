7db59a0ff4a2a93a9a72d2b51c26793b293aa92a
Maha Abo Assi <maha@onekw.com>
2025-04-21 00:19:11 +0400

    sdd smooth scroll

--- src/pages/menu/sunMenu.jsx
+++ src/pages/menu/sunMenu.jsx
@@ -12,10 +12,9 @@ function SubMenu() {
     useEffect(()=>{
         const temp = data.find(e => (e.id == id))
         setMenu(temp)
-        window.scrollTo({ top: 0, behavior: 'smooth' }); 
-
+        window.scrollTo({top: 0, left: 0, behavior: 'smooth'}); 
         
-    },[id])
+    },[])
     
     return ( <div>
         
